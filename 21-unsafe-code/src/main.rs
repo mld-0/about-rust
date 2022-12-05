544405bc0110131f79a9731cfaf2843af3889100
Matthew L Davis <mld.0@protonmail.com>
2022-12-06 00:08:31 +1100

    GapBuffer example
    Continue: assert correct GapBuffer behaviour, complete chapter.

--- 21-unsafe-code/src/main.rs
+++ 21-unsafe-code/src/main.rs
@@ -22,12 +22,14 @@
 //  Ongoing: 2022-12-04T23:34:46AEDT alignment of tuples (being smaller than their size?)
 //  Ongoing: 2022-12-04T23:58:14AEDT clairfy, 'offset' vs 'wrapping_offset' (book says the later is for going beyond end of the array (going by name alone, shouldn't that be the former?))
 //  Ongoing: 2022-12-05T21:47:15AEDT 'panic_safely_in_unsafe_code' (feels like a topic there is a lot more to say on)
+//  Ongoing: 2022-12-05T23:51:03AEDT how much of the comments in the book for example 'GapBuffer' are neccessary / not-redundent?
 //  }}}
 #![allow(unused)]
 #![allow(non_snake_case)]
 #![allow(non_camel_case_types)]
 
-//  Continue: 2022-12-05T21:40:51AEDT Gap buffer example
+//  Continue: 2022-12-06T00:04:08AEDT assert correct GapBuffer behaviour
+
 
 //  Use pointers only were references are inadequate (memory management / FFI [foreign-function-interfaces] / <>)
 //  References: shared '&' / mutable '&mut'
@@ -487,10 +489,139 @@ fn example_raw_pointers()
         //  Equivalent to C++ volatile read / write 
 
 
-        //  Example: Gap buffer
-        //  <>
+        //  <('std::ptr::drop_in_place()')>
+
     }
     moving_into_outOf_memory();
+
+
+    //  Example: Gap buffer
+    //  Emacs uses a gap buffer, which can insert/delete characters in constant time, as it keeps its spare capacity at the edit location.
+    //  (this example does not <consider/use/validate> UTF8)
+    mod gap {
+        use std;
+        use std::ops::Range;
+        pub struct GapBuffer<T> {
+            storage: Vec<T>,
+            gap: Range<usize>,
+        }
+        impl<T> GapBuffer<T> {
+            //  {{{
+            pub fn new() -> GapBuffer<T> {
+                GapBuffer { storage: Vec::new(), gap: 0..0 }
+            }
+            pub fn capacity(&self) -> usize { 
+                self.storage.capacity()
+            }
+            pub fn len(&self) -> usize {
+                self.capacity() - self.gap.len()
+            }
+            pub fn position(&self) -> usize {
+                self.gap.start
+            }
+            unsafe fn space(&self, index: usize) -> *const T {
+                self.storage.as_ptr().offset(index as isize)
+            }
+            unsafe fn space_mut(&mut self, index: usize) -> *mut T {
+                self.storage.as_mut_ptr().offset(index as isize)
+            }
+            fn index_to_raw(&self, index: usize) -> usize {
+                if index < self.gap.start {
+                    index
+                } else {
+                    index + self.gap.len()
+                }
+            }
+            pub fn get(&self, index: usize) -> Option<&T> {
+                let raw = self.index_to_raw(index);
+                if raw < self.capacity() {
+                    unsafe {
+                        Some(&*self.space(raw))
+                    }
+                } else {
+                    None
+                }
+            }
+            pub fn set_position(&mut self, pos: usize) {
+                if pos > self.len() {
+                    panic!("index=({}) out of range for GapBuffer", pos);
+                }
+                unsafe {
+                    let gap = self.gap.clone();
+                    if pos > gap.start {
+                        let distance = pos - gap.start;
+                        std::ptr::copy(self.space(gap.end), self.space_mut(gap.start), distance);
+                    } else if pos < gap.start {
+                        let distance = gap.start - pos;
+                        std::ptr::copy(self.space(pos), self.space_mut(gap.end-distance), distance);
+                    }
+                    self.gap = pos .. pos + gap.len();
+                }
+            }
+            pub fn insert(&mut self, elt: T) {
+                if self.gap.len() == 0 {
+                    self.enlarge_gap();
+                }
+                unsafe {
+                    let index = self.gap.start;
+                    std::ptr::write(self.space_mut(index), elt);
+                }
+                self.gap.start += 1;
+            }
+            pub fn insert_iter<I>(&mut self, iterable: I) 
+                where I: IntoIterator<Item=T>
+            {
+                for item in iterable {
+                    self.insert(item)
+                }
+            }
+            pub fn remove(&mut self) -> Option<T> {
+                if self.gap.end == self.capacity() {
+                    return None;
+                } 
+                let element = unsafe {
+                    std::ptr::read(self.space(self.gap.end))
+                };
+                self.gap.end += 1;
+                Some(element)
+            }
+            fn enlarge_gap(&mut self) {
+                let mut new_capacity = self.capacity() * 2;
+                if new_capacity == 0 {
+                    new_capacity = 4;
+                }
+                let mut new = Vec::with_capacity(new_capacity);
+                let after_gap = self.capacity() - self.gap.end;
+                let new_gap = self.gap.start .. new.capacity() - after_gap;
+                unsafe {
+                    std::ptr::copy_nonoverlapping(self.space(0), new.as_mut_ptr(), self.gap.start);
+                    let new_gap_end = new.as_mut_ptr().offset(new_gap.end as isize);
+                    std::ptr::copy_nonoverlapping(self.space(self.gap.end), new_gap_end, after_gap);
+                }
+                self.storage = new;
+                self.gap = new_gap;
+            }
+            fn drop(&mut self) {
+                unsafe {
+                    for i in 0 .. self.gap.start {
+                        std::ptr::drop_in_place(self.space_mut(i));
+                    }
+                    for i in self.gap.end .. self.capacity() {
+                        std::ptr::drop_in_place(self.space_mut(i));
+                    }
+                }
+            }
+            //  }}}
+        }
+    }
+
+    //  Usage:
+    use gap::GapBuffer;
+    let mut buf = GapBuffer::new();
+    buf.insert_iter("Lord of the Rings".chars());
+    buf.set_position(12);
+    buf.insert_iter("Onion".chars());
+    //  <(assert correct behaviour(?))>
 
 
     fn panic_safely_in_unsafe_code()
