f51097ef2ec1f3e89287d69689e629fba2f343bb
Matthew L Davis <mld.0@protonmail.com>
2023-01-14 19:48:09 +1100

    group by filename

--- _grab-ongoings.zsh
+++ _grab-ongoings.zsh
@@ -4,5 +4,15 @@ set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 
 re_date="[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}"
-grep -r --include "*.rs" "Ongoing: $re_date" | sort
+
+files=( `find . -name "*.rs" | sort` )
+
+for f in "${files[@]}"; do
+	matches=`grep "Ongoing: $re_date" $f | sed "s|^\s*//\s*||g"`
+	if [[ ! -z $matches ]]; then
+		echo "$f"
+		echo "$matches"
+		echo ""
+	fi
+done
 
