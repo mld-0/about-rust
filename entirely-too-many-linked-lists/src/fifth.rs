1d7ddfda3c2d7b210c54e3660e9aaad1bfc946d8
Matthew L Davis <mld.0@protonmail.com>
2023-02-01 19:53:41 +1100

    UnsafeCell/Box and UB, Box-less implementation
    Continue: pointer invalidation, debug Box implementation.

--- entirely-too-many-linked-lists/src/fifth.rs
+++ entirely-too-many-linked-lists/src/fifth.rs
@@ -27,11 +27,19 @@
 //  Ongoing: 2023-02-01T00:53:03AEDT modifying data through a shared reference is (always?) undefined?
 //  Ongoing: 2023-02-01T00:57:56AEDT stacked borrow - more concise defintion / list of what not to do
 //  Ongoing: 2023-02-01T01:01:33AEDT 'immutable reference' is a much more <meaningful/descriptive> term than 'shared reference'(?)
+//  Ongoing: 2023-02-01T18:06:28AEDT example_UB_interior_mutability could be clearer? (it really doesn't help when exercise says: "either Miri is imperfect and this is actually still UB, or the model we have discussed is a simpification")
+//  Ongoing: 2023-02-01T18:09:48AEDT UnsafeCell 'get()' / 'get_mut()' are terrible names(?) (should be get_ptr / get_mut?)
+//  Ongoing: 2023-02-01T18:28:50AEDT what is the Rust term for 'pointer invalidation'?
+//  Ongoing: 2023-02-01T18:31:05AEDT 'accessing a container invalidates any pointers to its contents' (do they mean dereferencing the container, or doing anything with the container?)
+//  Ongoing: 2023-02-01T18:37:45AEDT Box::into_raw() can cause a memory leak in safe Rust(?)
+//  Ongoing: 2023-02-01T18:43:30AEDT contention: exercise 6 solution is improved by eliminating 'Link<T>' and just replacing it with '*mut Node<T>' (or alternatively, replacing the former with the later - instead of using both like exercise does?) [...] (speaking of improving exercise answer - placing only unsafe calls in unsafe block instead of entire function?)
+//  Ongoing: 2023-02-01T18:57:30AEDT convert an array of values into an array of mutable pointers to those values?
 //  }}}
 use std::mem;
 use std::ptr;
 
-//  Continue: 2023-01-30T23:22:41AEDT complete exercise
+//  Continue: 2023-02-01T18:26:57AEDT pointer invalidation
+//  Continue: 2023-02-01T18:39:04AEDT debug UBList implementation
 
 //  On unsafe Rust (see below)
 //  LINK: https://doc.rust-lang.org/nightly/nomicon/
@@ -45,7 +53,7 @@ use std::ptr;
 
 
 //  Contention: performance guarantees are part of an interface
-//  (don't offer push_end/pop_end unless they are implemented efficiently)
+//  (don't offer push_end/pop_front unless they are implemented efficiently)
 
 //  Contention: raw pointers can be a prefereable alternative to Rc<RefCell>
 
@@ -298,18 +306,85 @@ fn example_UB_shared_references()
 {
     //  Using a shared reference to modify a value is undefined
     //  Once a shared reference is pushed onto the borrow stack, everything on top of it can only be shared
+
+    //  It is valid to create a mutable pointer from a shared reference, so long as we never try and use it to change the underlying value
+    unsafe {
+        let mut data = 10;
+        let mref1 = &mut data;
+        let ptr2 = mref1 as *mut i32;
+        let sref3 = &*mref1;
+        let ptr4 = sref3 as *const i32 as *mut i32;
+        //*ptr4 += 1;                                   //  UB: miri error, no compiler error
+        *ptr2 += 2;
+        *mref1 += 1;
+        assert_eq!(data, 13);
+    }
+
 }
 
 
 #[test]
 fn example_UB_interior_mutability()
 {
+    //  UnsafeCell is the core primative for interior mutablity in Rust
+    use std::cell::UnsafeCell;
+
+    //  get(&self) -> *mut T
+    //  Return a raw pointer to UnsafeCell contents
+
+    //  get_mut(&mut self) -> &mut T
+    //  Return a mutable reference to UnsafeCell contents
+
+    //  <(If we access UnsafeCell through a mutable reference, the compiler cannot make any assumptions about interior mutablity, and the usual rules about re-borrowing apply)>
+    //  invalid: 'ptr2' not in the borrow stack
+    //  unsafe {
+    //      let mut data = UnsafeCell::new(10_i32);
+    //      let mref1 = &mut data;
+    //      let ptr2 = mref1.get_mut() as *mut _;
+    //      *mref1.get() += 2;
+    //      *ptr2 += 1;
+    //      assert_eq!(*data.get_mut(), 13);
+    //  }
+
+    //  <(Recomendation: declare a reference to the UnsafeCell, and access pointer into cell by calling '.get()' on said reference)>
+    unsafe {
+        let mut data = UnsafeCell::new(10_i32);
+        let ref1 = &data;
+        let ptr2 = ref1.get();
+        *ref1.get() += 2;
+        *ptr2 += 1;                     //  note that 'ptr2' is used *after* 'ref1'
+        assert_eq!(*data.get_mut(), 13);
+    }
+
+    //  <(contention: the compiler is better able to understand our use of UnsafeCell if we access its contents through pointers '.get()' instead of mutable references '.get_mut()')>
 }
 
 
 #[test]
 fn example_UB_Box()
 {
+    //  Box<T> claims unique ownership of the memory it points to
+    //  (it does not play nicely with raw pointers)
+
+    //  invalid: tag does not exist in borrow stack
+    //  <(we invalidate 'ptr1' by dereferencing 'data')>
+    //  unsafe {
+    //      let mut data = Box::new(10);
+    //      let ptr1 = data.as_mut() as *mut i32;
+    //      *data += 10;
+    //      *ptr1 += 1;
+    //      assert_eq!(*data, 21);
+    //  }
+    //
+    //  valid:
+    unsafe {
+        let mut data = Box::new(10);
+        let ptr1 = data.as_mut() as *mut i32;
+        *ptr1 += 1;
+        *data += 10;
+        assert_eq!(*data, 21);
+    }
+
 }
 
 
@@ -404,19 +479,132 @@ impl<T> UBList<T> {
 //  }}}
 
 
+//  'Box::into_raw()'
+//  Consumes the box, returning a wrapped raw pointer
+//  The caller is responsibly for the memory previously managed by the box
+//  (This is most easily done by converting the raw pointer back into a box with 'Box::from_raw()' and dropping the box)
+#[test]
+fn example_Box_into_raw()
+{
+    let x = Box::new(String::from("hello"));
+    let p = Box::into_raw(x);
+    let x = unsafe { Box::from_raw(p) };
+}
+
+
 //  Contention: when using raw pointers, try to *only* use raw pointers
 //  Mixing safe and raw pointers is a recipe for UB
 //  (Accessing a container by-val/ref invalidates any pointers to its contents) (even though program that does may still work (making testing with Miri essential for any code that uses unsafe))
 //  Since raw pointers are null-able, they do not need to be wrapped in Option
 
-type Link<T> = *mut Node<T>;
 pub struct List<T> {
-    head: Link<T>,
+    head: *mut Node<T>,
     tail: *mut Node<T>,
 }
 struct Node<T> {
     elem: T,
-    next: Link<T>,
+    next: *mut Node<T>,
+}
+impl<T> List<T> {
+    pub fn new() -> Self {
+        List { head: ptr::null_mut(), tail: ptr::null_mut(), }
+    }
+    pub fn push_end(&mut self, elem: T) {
+        let new_tail = Box::into_raw(Box::new(Node {
+            elem, next: ptr::null_mut(), 
+        }));
+        if !self.tail.is_null() {
+            unsafe { (*self.tail).next = new_tail };
+        } else {
+            self.head = new_tail;
+        }
+        self.tail = new_tail;
+    }
+    pub fn pop_front(&mut self) -> Option<T> {
+        if self.head.is_null() {
+            None
+        } else {
+            let head = unsafe { Box::from_raw(self.head) };
+            self.head = head.next;
+            if self.head.is_null() {
+                self.tail = ptr::null_mut();
+            }
+            Some(head.elem)
+        }
+    }
+    pub fn peek(&self) -> Option<&T> {
+        unsafe { self.head.as_ref().map(|node| &node.elem ) }
+    }
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        unsafe { self.head.as_mut().map(|node| &mut node.elem ) }
+    }
+}
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        while let Some(_) = self.pop_front() {}
+    }
+}
+
+//  'ptr::as_ref()'
+//  'ptr::as_mut()'
+//  (unsafe functions), return Option containing a shared/mutable reference to the value being pointed at
+//  contention: usually these are functions to avoid
+//  The lifetime of the returned reference is unbounded - not determined by the lifetime of the pointer
+
+
+pub struct IntoIter<T>(List<T>);
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop_front()
+    }
+}
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+
+pub struct Iter<'a, T> { 
+    next: Option<&'a Node<T>>, 
+}
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe {
+            self.next.map(|node| {
+                self.next = node.next.as_ref();
+                &node.elem
+            })
+        }
+    }
+}
+impl<T> List<T> {
+    pub fn iter(&self) -> Iter<'_, T> {
+        unsafe { Iter { next: self.head.as_ref() } }
+    }
+}
+
+
+pub struct IterMut<'a, T> { 
+    next: Option<&'a mut Node<T>>, 
+}
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+    fn next(&mut self) -> Option<Self::Item> {
+        unsafe {
+            self.next.take().map(|node| {
+                self.next = node.next.as_mut();
+                &mut node.elem
+            })
+        }
+    }
+}
+impl<T> List<T> {
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        unsafe { IterMut { next: self.head.as_mut() } }
+    }
 }
 
 
@@ -425,6 +613,7 @@ struct Node<T> {
 mod test {
 
     //use super::UBList;
+    //  {{{
     //#[test] 
     //fn basics_UB() {
     //    let mut list = UBList::new();
@@ -438,6 +627,118 @@ mod test {
     //    assert_eq!(list.pop_front(), Some(5));
     //    assert_eq!(list.pop_front(), None);
     //}
+    //  }}}
+
+    use super::List;
+
+    #[test]
+    fn push_and_pop() {
+        let mut list = List::new();
+        assert_eq!(list.pop_front(), None);
+        list.push_end(1); 
+        list.push_end(2); 
+        list.push_end(3);
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), Some(2));
+        list.push_end(4); 
+        list.push_end(5);
+        assert_eq!(list.pop_front(), Some(3));
+        assert_eq!(list.pop_front(), Some(4));
+        assert_eq!(list.pop_front(), Some(5));
+        assert_eq!(list.pop_front(), None);
+        list.push_end(6); 
+        list.push_end(7);
+        assert_eq!(list.pop_front(), Some(6));
+        assert_eq!(list.pop_front(), Some(7));
+        assert_eq!(list.pop_front(), None);
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push_end(1); list.push_end(2); list.push_end(3);
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), None);
+        let mut list = List::new();
+        list.push_end(1); list.push_end(2); list.push_end(3);
+        let vals = vec![1,2,3];
+        for (x,check) in list.into_iter().zip(vals.into_iter()) {
+            assert_eq!(x, check);
+        }
+    }
+
+    #[test]
+    fn iter() {
+        let mut list = List::new();
+        list.push_end(1); list.push_end(2); list.push_end(3);
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&1));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut list = List::new();
+        list.push_end(1); 
+        list.push_end(2); 
+        list.push_end(3);
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 1));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+        list.push_end(1); 
+        list.push_end(2); 
+        list.push_end(3);
+        assert_eq!(list.peek(), Some(&1));
+        assert_eq!(list.peek_mut(), Some(&mut 1));
+        list.peek_mut().map(|x| { *x = 42 });
+        assert_eq!(list.peek(), Some(&42));
+        assert_eq!(list.peek_mut(), Some(&mut 42));
+    }
+
+    #[test]
+    fn miri_food()
+    {
+        let mut list = List::new();
+        list.push_end(1);
+        list.push_end(2);
+        list.push_end(3);
+        assert_eq!(list.pop_front(), Some(1));
+        list.push_end(4);
+        assert_eq!(list.pop_front(), Some(2));
+        list.push_end(5);
+        assert_eq!(list.peek(), Some(&3));
+        list.push_end(6);
+        list.peek_mut().map(|x| *x *= 10);
+        assert_eq!(list.peek(), Some(&30));
+        assert_eq!(list.pop_front(), Some(30));
+        for x in list.iter_mut() {
+            *x *= 100;
+        }
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&400));
+        assert_eq!(iter.next(), Some(&500));
+        assert_eq!(iter.next(), Some(&600));
+        assert_eq!(iter.next(), None);
+        assert_eq!(iter.next(), None);
+        assert_eq!(list.pop_front(), Some(400));
+        list.peek_mut().map(|x| *x *= 10);
+        assert_eq!(list.peek(), Some(&5000));
+        list.push_end(7);
+    }
 
 }
 
