204fa3cc01b20c21d51928c3f62ff268c57010bc
Matthew L Davis <mld.0@protonmail.com>
2023-02-16 21:59:35 +1100

    casts
    Continue: complete chapter.

--- dark-arts-of-unsafe/src/type_conversions.rs
+++ dark-arts-of-unsafe/src/type_conversions.rs
@@ -12,6 +12,12 @@
 //  Ongoing: 2023-02-15T23:36:13AEDT Nothing in Rust prevents a trait from having a method with the same name as another trait's method, nor does Rust prevent you from implementing both traits on one type ... (hence "Fully Qualified Syntax for Disambiguation") ... (Rust rules on multiple methods of the same name?)
 //  Ongoing: 2023-02-15T23:43:25AEDT '<T: Cloned>' is a "trait bound"
 //  Ongoing: 2023-02-16T00:04:13AEDT 'autoref': <(when '&self' / '&mut self' is <implicitly> inserted as first function argument)>
+//  Ongoing: 2023-02-16T21:30:34AEDT 'where V: Sized' (means V is *not* optionally sized?)
+//  Ongoing: 2023-02-16T21:42:01AEDT on overflow (when casting int->float)? [...] (from the very next line: 'with the current set of numeric types, overflow can only happen on u128 as f32 for values greater or equal to f32::MAX + (0.5 ULP)')
+//  Ongoing: 2023-02-16T21:45:09AEDT 'casts involving rounding (int->float / f64->f32) are slow on unsupported hardware' ... (just how old/basic are we talking?)
+//  Ongoing: 2023-02-16T21:54:47AEDT meaning of term 'true cast'?
+//  Ongoing: 2023-02-16T21:56:48AEDT talking about corner cases of <pointer> casts -> (like what?)
+//  Ongoing: 2023-02-16T21:57:29AEDT meaning of 'raw slice' (a slice without a length) (or does it refer to the fact these are pointers to slices eg: '*const [u16]')?
 //  }}}
 use std::sync::Arc;
 
@@ -94,12 +100,11 @@ fn dot_operator()
     //  Example: <?> with auto-derived / custom 'Clone' implementation
     //  <(where auto behaviour for auto-derived function is <wrong?>)>
     //
-    #[derive(Clone)]
-    struct MyContainer_i<T>(Arc<T>);
-    //
     //  <(auto-derived 'Clone' implementation equivalent to:)>
+    //  <(contention: this is equivalent to the 'derive(Clone)' implementation
     struct MyContainer_ii<T>(Arc<T>);
-    impl<T> Clone for MyContainer_ii<T> where T: Clone
+    impl<T> Clone for MyContainer_ii<T> 
+        where T: Clone 
     {
         fn clone(&self) -> Self {
             Self( Arc::clone(&self.0) )
@@ -116,23 +121,17 @@ fn dot_operator()
     }
     //  
     //  <(Usage:)>
-    fn clone_mycontainer_i<T>(foo: &MyContainer_i<i32>, bar: &MyContainer_i<T>) {
-        //  <(calls?)>
-        let foo_cloned = foo.clone();       //  T = <?>
-        //  <(calls?)>
-        let bar_cloned = bar.clone();       //  T = <?>
-    }
     fn clone_mycontainer_ii<T>(foo: &MyContainer_ii<i32>, bar: &MyContainer_ii<T>) {
         //  <(calls?)>
-        let foo_cloned = foo.clone();       //  T = <?>
+        let foo_cloned = foo.clone();       //  T = MyContainer_ii<i32>
         //  <(calls?)>
-        let bar_cloned = bar.clone();       //  T = <?>
+        let bar_cloned = bar.clone();       //  T = &MyContainer_ii<T>
     }
     fn clone_mycontainer_iii<T>(foo: &MyContainer_iii<i32>, bar: &MyContainer_iii<T>) {
         //  <(calls?)>
-        let foo_cloned = foo.clone();       //  T = <?>
+        let foo_cloned = foo.clone();       //  T = MyContainer_iii<i32>
         //  <(calls?)>
-        let bar_cloned = bar.clone();       //  T = <?>
+        let bar_cloned = bar.clone();       //  T = MyContainer_iii<T>
     }
 
 
@@ -146,11 +145,83 @@ fn dot_operator()
 #[test]
 fn casts()
 {
+    //  Casts are a superset of coercisons: every coercison can be explicitly invoked via cast
+
+    //  Pointer casts are infallible at runtime. There will be no indication/error if such a cast triggers a corner case.
+
+    //  Lengths are not adjusted when casting raw slices:
+    //          *const [u16] as *const [u8]
+    //  produces a slice that only includes half the origional memory
+
+    //  Casting is not transitive
+    //  Just because 'e as U1 as U2' is valid doesn't necessarily mean 'e as U2' is valid
+
+    //  LINK: https://doc.rust-lang.org/nightly/reference/expressions/operator-expr.html#type-cast-expressions
+    //  {{{
+
+    //  TypeCastExpression:
+    //          Expression as TypeNoBounds
+
+    //  Valid casts:
+    //          int/float               int/float
+    //          enum                    int
+    //          bool/char               int
+    //          u8                      char
+    //          *T                      *V (where V: Sized) [1]
+    //          *T (where T: Sized)     int
+    //          int                     *V (where V: Sized)
+    //          &[mut] T                *[mut] T
+    //          *[mut] [T; n]           *[mut] T
+    //          <Function item>         <Function pointer>
+    //          <Function item>         *V (where V: Sized)
+    //          <Function item>         int
+    //          <Function pointer>      *V (where V: Sized)
+    //          <Function pointer>      int
+    //          <Closure> [2]           <Function pointer>
+    //  [1]: or T/V are compatible unsized types (eg: slices, same-trait-object)
+    //  [2]: Closure must not capture any local variables
+
+    //  <(Function item: <?>)>
+
+    //  <(obtaining a mut pointer from a const ref is allowed, using it to modify said value is UB)>
+
+    //  Integer casts:
+    //          casting between integers of the same size (eg: i32->u32) is a no-op
+    //          casting from larger->smaller int will truncate
+    //          casting unsigned smaller->larger int will zero-extend
+    //          casting signed smaller->larger int will sign-extend
+
+    //  Float casts:
+    //          float->int with decimals will round towards zero
+    //          float->int where float > intmax will become intmax
+    //          float->int where float < intmin will become intmin
+    //          int->float will produce the closet possible float
+    //          int->float will produce +/- inf on overflow
+    //          f32->f64 is perfect and lossless
+    //          f64->f32 will produce the closest possible f32
+    //          f64->f32 will produce +/- inf on overflow
+    //  (int->float and f64->f32 casts involving rounding are slow on unsupported hardware)
+
+    //  Enum casts are limited to:
+    //          unit-only enums
+    //          field-less enums (without explicit discriminates)
+
+    //  Primative casts:
+    //          bool casts to 0/1
+    //          char casts to the value of the code point (then uses a numeric cast if needed)
+
+    //  u8 casts:
+    //          to char with corresponding code point
+
+    //  <(Rust's memory model is still under development)>
+
+    //  }}}
 }
 
 
 #[test]
 fn transmutes()
 {
+
 }
 
