aefa5d06981ba93ca8adcb8c1b38f35cc9bbce49
mld-0 <mld.0@protonmail.com>
2023-02-23 00:00:36 +1100

    panic
    Continue: complete chapter.

--- dark-arts-of-unsafe/src/unwinding.rs
+++ dark-arts-of-unsafe/src/unwinding.rs
@@ -8,6 +8,8 @@
 //  Ongoings:
 //  {{{
 //  Ongoing: 2023-02-21T22:52:06AEDT add (all items) "func_name: DONE" messages?
+//  Ongoing: 2023-02-22T23:42:31AEDT 'compile_error!()' example?
+//  Ongoing: 2023-02-22T23:59:05AEDT Result vs Option: Result can contain an error type, (<what/anything> else?)
 //  }}}
 
 //  Continue: 2023-02-21T22:52:40AEDT complete chapter.
@@ -29,12 +31,31 @@
 fn panic()
 {
     //  LINK: https://doc.rust-lang.org/std/macro.panic.html
-    //  {{{
-    //  }}}
+    //
+    //  'panic!()'
+    //  Allows the program to terminate immediately with feedback to the user
+    //  Takes a string argument using 'format!()' syntax
+    //  Default behaviour is to print message to stderr with file/line/column of panic call
+    //
+    //  Set default behaviour with 'std::panic::set_hook()'
+    //  Use 'std::panic::panic_any()' to panic with any other type as argument
+    //  Use 'compile_error!()' for conditional errors at compile time
+    //
+    //  panic vs Result
+    //  panic is used to construct errors representing a bug
+    //  Result is used to wrap the result of an action that can fail
+    //  Propogate any error in a Result with the '?' operator
 
     //  LINK: https://doc.rust-lang.org/book/ch09-03-to-panic-or-not-to-panic.html
-    //  {{{
-    //  }}}
+    //
+    //  <('expect' is just 'unwrap' that takes an argument)>
+    //
+    //  A final program should have more robust error handling than 'unwrap' / 'expect' where failure is anticipated
+    //  
+    //  Use 'result.expect(msg)' to indicate that Err would be a bug
+    //
+    //  panic when something happens that could leave the program in a bad state
+    //  panic when a function is given input it cannot handle
 }
 
 
@@ -86,10 +107,25 @@ fn catch_unwind()
     assert!(result.is_err());
 
     //  Unwinding is slow, as it is not meant to be used under normal circumstances
+    //  (Rust optimises heavily for the "doesn't unwind" case)
 
     //  It is UB to unwind from Rust code into foreign code
     //  (use 'catch_unwind' to catch any panics <at/before> the FFI boundry)
+
+
+    //  Example: destructors called
+    //  <>
+    struct Foo {};
+    impl Drop for Foo {
+        fn drop(&mut self) {
+            println!("drop Foo");
+        }
+    }
+    panic::catch_unwind( || {
+        let x = Foo {};
+    });
 }
+
 
 
 #[test]
@@ -102,3 +138,4 @@ fn exception_safety()
 fn poisoning()
 {
 }
+
