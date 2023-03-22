c6738b3008afe58806bff6fa70e1627efcb3e4e2
mld-0 <mld.0@protonmail.com>
2023-03-22 19:58:24 +1100

    atomic accesses
    Sequentially-Consistent/Acquire-Release/Relaxed atomic ordering.
    Explanations are perfunctory - far from 'how to do concurrency' guide.
    Continue: extra resources items.

--- dark-arts-of-unsafe/src/concurrency.rs
+++ dark-arts-of-unsafe/src/concurrency.rs
@@ -7,7 +7,7 @@
 #![allow(non_camel_case_types)]
 #![feature(negative_impls)]
 use std::thread;
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::atomic::{AtomicUsize, AtomicBool, Ordering};
 use std::sync::Arc;
 //  Ongoings:
 //  {{{
@@ -15,6 +15,9 @@ use std::sync::Arc;
 //  Ongoing: 2023-03-01T02:01:27AEDT claim: Send/Sync are the *only* automatically derived traits(?)
 //  Ongoing: 2023-03-01T02:10:17AEDT compiler doesn't object to manually implementing either Send/Sync (for a type for which neither is automatically derived) without the other
 //  Ongoing: 2023-03-01T02:19:06AEDT which standard library containers</other-types> are Send/Sync?
+//  Ongoing: 2023-03-22T19:47:56AEDT 'Arc.compare_and_swap()' is deprecate (use compare_exchange/compare_exchange_weak instead (which is not a drop-in replacement -> it expects a Result)
+//  Ongoing: 2023-03-22T19:54:12AEDT 'atomics' item doesn't <really/actually> explain how to use the three types of atomic accesses given
+//  Ongoing: 2023-03-22T19:55:24AEDT C++ equivalents for SeqCst/Acquire-Release/Relaxed atomic accesses ((do they exist and) how similar are they Rust -> C++ examples doubtless have far more <examples/resources> available)
 //  }}}
 
 //  Continue: 2023-02-28T23:43:05AEDT data_race_conditions, explanations for Examples 1/2, demonstration of race/error
@@ -80,7 +83,7 @@ fn send_and_sync()
     //  Sync: a type is safe to share between threads (T is Sync iff &T is Send)
     //  These are marker traits (they do not have associated functions/data)
 
-    //  These are auto traits: any type comprised entirely of Send/Sync types is Send/Sync
+    //  These are auto traits: any type comprised entirely of Send/Sync types is <(automatically)> Send/Sync
     //  Most primitives are both Send/Sync (meaning most types are Send/Sync)
     //  Exceptions: 
     //          raw pointers are neither Send/Sync
@@ -95,7 +98,7 @@ fn send_and_sync()
 
     struct SpecialThreadToken(u8);
 
-    //  We can also unimplement Send/Sync for a type which automatically derives it
+    //  We can also un-implement Send/Sync for a type which automatically derives it
     //
     //  <(not available (even in nightly?) method: negative_impls)>
 //  #![feature(negative_impls)]
@@ -107,7 +110,7 @@ fn send_and_sync()
     use core::cell::Cell;
     struct SpecialThreadToken_ii(u8, PhantomData<Cell<()>>);
 
-    //  Most types that use raw pointers should be sufficently encapsulated that Send/Sync can be derived
+    //  Most types that use raw pointers should be sufficiently encapsulated that Send/Sync can be derived
     //  <(all of Rust's standard collections are Send/Sync (when instantiated for Send/Sync types))>
 
 
@@ -119,7 +122,63 @@ fn send_and_sync()
 #[test]
 fn atomics()
 {
-    //  <>
+    //  Rust inherits the C++20 memory model for atomics
+    //  (This model is far from perfect, but so are most memory models for atomics, and this one has the benefit of being widely used)
+
+    //  The cache hierarchies and out-of-order execution of modern hardware can result in the same events occurring in different orders on different threads. 
+    //  Different hardware can offer different guarantees
+    //          x86 provides strong ordering guarantees
+    //          ARM provides weak ordering guarantees
+    //  Asking for stronger guarantees (using specialised synchronization instructions) can be more expensive on weakly-ordered hardware than strongly-ordered hardware
+
+    //  The C++ memory model talks about 'causality' of a program
+    //  Generally by establishing 'happens-before' relationships between parts of the program
+
+
+    //  'Data accesses' are fundamentally unsynchronized
+    //  The compiler is free to aggressively optimize these (on the assumption the program is single threaded)
+    //  It is impossible to write correctly synchronized multi-threaded code using only data accesses
+
+
+    //  'Atomic accesses' are marked with an ordering specifying their relationships with other accesses
+    //  Rust provides:
+    //          Sequentially Consistent (SeqCst)
+    //          Release
+    //          Acquire
+    //          Relaxed
+    //  (C++'s 'consume ordering' is not available)
+
+
+    //  Sequentially Consistent 
+    //  Most powerful atomic ordering
+    //  These operations cannot be reordered: all accesses that happen before/after a SeqCst access stay before/after it
+    //  This is rarely necessary for program correctness, and is generally slower than other memory orders
+    //  It is trivial to downgrade 'SeqCst' to 'Relaxed' at a later point (although proving the program is still correct after such a downgrade can be less trivial)
+    //  If a program is free of data-races, and uses only SeqCst-accesses and data-accesses, then it will have a single global order of execution that all threads agree on (this is not true of the weaker atomic orderings)
+
+
+    //  Acquire-Release
+    //  Acquire and Release are largely intended to be paired
+    //  They can be used for acquiring/releasing locks and ensuring critical sections do not overlap
+    //  (Accesses between an acquire/release are kept between the acquire/release, but accesses before/after are free to be re-ordered to in-between)
+    //  Causality is established when thread A releases a memory location and thread B subsequently acquires the same location in memory
+    //
+    //  Basic usage: acquire a location of memory to begin the critical section, and release that location to end it
+    let lock = Arc::new(AtomicBool::new(false));        //  "am I locked"
+    //  ... distribute lock to threads
+    while lock.compare_and_swap(false, true, Ordering::Acquire) { }     //  <deprecated>
+    //while lock.compare_exchange(false, true, Ordering::Acquire) { }   //  <(how to use?)>
+    //  successfully acquired the lock when the loop ends
+    //  ... critical data accesses ...
+    lock.store(false, Ordering::Release);               //  release the lock
+
+
+    //  Relaxed
+    //  Weakest atomic ordering
+    //  Any read-modify-write operations done occur atomically
+    //  May not be faster than Acquire-Release on strongly ordered platforms
+    //  <(appropriate for things that you definitely want to happen, but don't particularly otherwise care about)>
+    //  Usecase: use 'fetch_add' to increment a counter shared by multiple threads
 }
 
 #[test]
