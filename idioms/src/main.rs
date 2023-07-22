c5d269229ffc8722d90cae5de5adc015c6249db9
mld-0 <mld.0@protonmail.com>
2023-07-22 20:47:47 +1000

    ongoing

--- idioms/src/main.rs
+++ idioms/src/main.rs
@@ -5,14 +5,59 @@
 //  Ongoings:
 //  {{{
 //  }}}
-#![allow(unused)]
+//#![allow(unused)]
 #![allow(non_snake_case)]
+
+
+fn about_match() 
+{
+}
+
+
+fn if_let()
+{
+}
+
+
+fn while_let()
+{
+}
+
+
+fn about_enums()
+{
+}
 
 
 //  Option<T>
 fn Option_T()
 {
+    //  LINK: Module std::option (see below)
+    //  https://doc.rust-lang.org/std/option/
+
+    //  is_some()           
+    //  is_none()
+
+    //  Adapters for working with references:
+    //  as_ref():               &Option<T> -> Option<&T>
+    //  as_mut()                &mut Option<T> -> Option<&mut T>
+    //
+    //  as_deref():             &Option<T> -> Option<&T::Target>
+    //  as_deref_mut():         &mut Option<T> -> Option<&mut T::Target>
+
+
+    //  Extract contained value:
+    //  expect():               <>
+    //  unwrap():               <>
+    //  unwrap_or():            <>
+    //  unwrap_or_default()     <>
+    //  unwrap_or_else()        <>
+
+    //  Transform contained value:
+    //  <>
+
 }
+
 
 //  Box<T>
 fn Box_T()
@@ -27,5 +72,13 @@ fn Option_Box_T()
 
 fn main() 
 {
-    println!("Hello, world!");
+    about_match();
+    about_enums();
+    if_let();
+    while_let();
+    Option_T();
+    Box_T();
+    Option_Box_T();
+    println!("main(), DONE");
 }
+
