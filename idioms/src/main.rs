56ee68d9eb31016a12cbebafbdf81e4ae88b04e1
mld-0 <mld.0@protonmail.com>
2023-07-22 20:34:42 +1000

    'idioms' - for clarifying Rust concepts

---
+++ idioms/src/main.rs
@@ -0,0 +1,31 @@
+//  {{{3
+//  vim: set tabstop=4 modeline modelines=10:
+//  vim: set foldlevel=2 foldcolumn=2 foldmethod=marker:
+//  {{{2
+//  Ongoings:
+//  {{{
+//  }}}
+#![allow(unused)]
+#![allow(non_snake_case)]
+
+
+//  Option<T>
+fn Option_T()
+{
+}
+
+//  Box<T>
+fn Box_T()
+{
+}
+
+//  Option<Box<T>>
+fn Option_Box_T()
+{
+}
+
+
+fn main() 
+{
+    println!("Hello, world!");
+}
